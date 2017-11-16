f153031934ccc4e24fb37a5309c23a06d8b764e8
edel <edel.altares@gmail.com>
2017-11-15 21:54:56 -0700

    Check if rule is correct

--- fw.py
+++ fw.py
@@ -26,23 +26,68 @@ class Firewall:
 
         return filename
 
+    def valid_rule(self, rule):
+        """ Check if the rule is valid """
+        flag = True
+
+        if len(rule) > 5 or len(rule) < 4:
+            return False
+
+
+        direction = str(rule[0])
+        action = str(rule[1])
+        ip = str(rule[2])
+        ports_og = str(rule[3])
+        ports = str(rule[3]).split(",")
+        established = str(rule[-1])
+
+        # check if direction is correct
+        if direction not in ("in", "out"):
+            flag = False
+
+        # check if action is correct
+        if action not in ("deny", "accept", "reject"):
+            flag = False
+
+        # check if IP is correct
+        if len(ip.split(".")) != 4 and ip != "*":
+            flag = False
+            
+        # check if each port is good
+        for port in ports:
+            if port == "*":
+                continue
+            elif int(port) not in range(0,65535):
+                flag = False
+
+        # check if flag is good
+        if established != ports_og and established != "established":
+            flag = False
+
+        return flag
+
+
     def parse_file(self, filename):
         """ Parse the file """
 
         # go through each line in the file
         with open(filename, 'r') as file:
+            count = 0
 
             for line in file:
                 # split line with whitespace
                 rule_data = line.strip().split()
 
-                # check if the line read in was accurate
-                if len(rule_data) != 4 or len(rule_data) !=5:
-                    sys.stderr("Warning: An invalid rule was encountered")
-                    pass
+                # rule is not valid
+                if not self.valid_rule(rule_data):
+                    sys.stderr.write(str(count) + " Warning: An invalid rule was encountered\n")
 
-                # TODO
-                print(rule_data)
+                # rule is valid
+                else:
+                    # TODO
+                    # print(rule_data)
+
+                    count += 1
 
         return
 
@@ -63,8 +108,5 @@ class Firewall:
 
 
 if __name__ == "__main__":
-    try:
-        fw = Firewall()
-        fw.run()
-    except Exception as e:
-        print("Error: " + str(e))
+    fw = Firewall()
+    fw.run()
